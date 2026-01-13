65499cc21d33146ed0fe1e6934cfd100f180cada
Kevin Darmawan <kevindarmawan022@gmail.com>
2026-01-13 21:00:34 +0700

    feat: added relation field

---
+++ app/FieldOptions/RelationFieldOption.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\FieldOptions;
+
+use App\Contracts\CollectionFieldOption;
+use App\Models\Collection;
+
+class RelationFieldOption implements CollectionFieldOption
+{
+    public function __construct(
+        public string $collection,
+        public bool $cascadeDelete = false,
+        public bool $multiple = false,
+        public int|string|null $minSelect,
+        public int|string|null $maxSelect,
+    ) {}
+
+    public function toArray(): array
+    {
+        return [
+            'collection' => $this->collection,
+            'multiple' => $this->multiple,
+            'cascadeDelete' => $this->cascadeDelete,
+            'minSelect' => $this->minSelect,
+            'maxSelect' => $this->maxSelect,
+        ];
+    }
+
+    public static function fromArray(array $data): static
+    {
+        return new static(
+            collection: $data['collection'] ?? '',
+            multiple: $data['multiple'] ?? false,
+            cascadeDelete: $data['cascadeDelete'] ?? false,
+            minSelect: $data['minSelect'] ?? null,
+            maxSelect: $data['maxSelect'] ?? null,
+        );
+    }
+
+    public function validate(): bool
+    {
+        return true;
+    }
+
+    public function getValidationRules(): array
+    {
+        // $availableCollections = Collection::pluck('name')->toArray();
+        return [
+            // 'collection' => 'required|string',
+            'multiple' => 'boolean',
+            'cascadeDelete' => 'boolean',
+            'minSelect' => ['nullable', 'integer'],
+            'maxSelect' => ['nullable', 'integer'],
+        ];
+    }
+
+    public function getValidationMessages(): array
+    {
+        return [];
+    }
+}
